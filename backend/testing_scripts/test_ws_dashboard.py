8ad1936cad1902160eb4dc5a41d378e7604df22d
Avinash <avinashchintu789@gmail.com>
2026-09-11 15:50:48 +0530

    test dashboard token updated

--- backend/testing_scripts/test_ws_dashboard.py
+++ backend/testing_scripts/test_ws_dashboard.py
@@ -1,7 +1,7 @@
 import asyncio
 import websockets
 
-ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwiZXhwIjoxNzg3NDE3ODg0LCJ0eXBlIjoiYWNjZXNzIn0.RHfONbfexIG5lPJ16X6gFl_Q83XjnvktiQM10jzH-7U"  # ← replace with your value from Step 0C
+ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwiZXhwIjoxNzg5MTIxODQzLCJ0eXBlIjoiYWNjZXNzIn0.y8WCsA2w053tNrvXS7fc2VOeWUsvbDTva2aHR7dAeMc"  # ← replace with your value from Step 0C
 
 async def main():
     uri = f"ws://127.0.0.1:8000/ws/dashboard?token={ACCESS_TOKEN}"
